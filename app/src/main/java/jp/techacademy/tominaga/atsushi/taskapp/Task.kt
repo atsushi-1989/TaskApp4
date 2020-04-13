54687266a222b18ba69966e98d9ff6d691ab22ff
atsushi-1989 <atushitomiaga5@gmail.com>
2020-04-13 11:49:01 +0900

    TaskApp4 起動不可

---
+++ app/src/main/java/jp/techacademy/tominaga/atsushi/taskapp/Task.kt
@@ -0,0 +1,18 @@
+package jp.techacademy.tominaga.atsushi.taskapp
+
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.io.Serializable
+import java.util.Date
+
+
+open class Task: RealmObject(), Serializable {
+    var title: String = ""  //タイトル
+    var contents: String = "" //内容
+    var date: Date = Date()  //日付
+
+    //id をプライマリーキーとして設定
+    @PrimaryKey
+    var id: Int = 0
+}
